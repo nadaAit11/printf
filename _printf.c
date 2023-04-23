0b9699b1563e8a7392555e5f3cc54c610585553e
Nada <nadaait171@gmail.com>
2023-04-23 15:14:04 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,5 +1,9 @@
 #include "main.h"
 
+#define BUFFER_SIZE 1024
+
+void flush_buffer(char buffer[], int *buff_ind);
+
 /**
  * _printf - produces output according to a format
  * @format: a character string
@@ -9,56 +13,47 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
+	int buff_ind = 0;
 	int count = 0;
-	char c;
+	char buffer[BUFFER_SIZE];
 
 	va_start(args, format);
 
-	while (*format != '\0')
+	if (format == NULL)
+		return (-1);
+
+	for (int i = 0 ; format[i] != '\0' ; i++)
 	{
-		if (*format == '%')
-		{
-			format++;
-			switch (*format)
-			{
-				case 'c' :
-				{
-					c = va_arg(args, int);
-					putchar(c);
-					count++;
-					break;
-				}
-				case 's' :
-				{
-					for (char *s = va_arg(args, char *); *s != '\0' ; s++)
-					{
-						putchar(*s);
-						count++;
-					}
-					break;
-				}
-				case '%' :
-				{
-					putchar('%');
-					count ++;
-					break;
-				}
-				default : 
-				{
-					putchar('%');
-					putchar(*format);
-					count += 2;
-					break;
-				}
-			}
-		}
+		if (format[i] == '%')
+			i++;
 		else
 		{
-			putchar(*format);
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFFER_SIZE)
+			{
+				flush_buffer(buffer, &buff_ind);
+			}
 			count++;
 		}
-		format++;
 	}
+	flush_buffer(buffer, &buff_ind);
+
 	va_end(args);
+
 	return (count);
+}
+/**
+ * flush_buffer - prints contents of the buffer using putchar
+ * @buffer: array of characters
+ * @buff_ind: the size of the buffer
+ * Return: nothing (void)
+ */
+
+void flush_buffer(char buffer[], int *buff_ind)
+{
+	for (int i = 0 ; i < *buff_ind ; i++)
+	{
+		putchar(buffer[i]);
+	}
+	*buff_ind = 0;
 }
