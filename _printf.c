507c25b56d206973ee15a8dfe3b7e638f586320b
Nada <nadaait171@gmail.com>
2023-04-24 18:24:25 -0700

    :)

--- _printf.c
+++ _printf.c
@@ -27,7 +27,7 @@ int _printf(const char * const format, ...)
 Here:
 	while (format[i] != '\0')
 	{
-		j = 13;
+		j = 14;
 		while (j >= 0)
 		{
 			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
